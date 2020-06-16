6b0042586e48c0177bc8f77641777fc073635da4
kasimozdemir <kasim.ozdemir@icisleri.gov.tr>
2020-06-16 10:56:51 +0300

    Initial Commit

---
+++ FirebaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by KasimOzdemir on 16.06.2020.
+//  Copyright © 2020 KasimOzdemir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
